49c378c345444548ca9b6efa25ce6223349fab72
Ruan <ruan@poussee.com.br>
2020-01-22 16:14:53 -0300

    first deloy

---
+++ Scripts/main.js
@@ -0,0 +1,17 @@
+/**
+ *
+ *  sticky navigation
+ *
+ */
+
+let navbar = $(".navbar");
+
+$(window).scroll(function () {
+  // get the complete hight of window
+  let oTop = $(".buy").offset().top - window.innerHeight;
+  if ($(window).scrollTop() > oTop) {
+    navbar.addClass("sticky");
+  } else {
+    navbar.removeClass("sticky");
+  }
+});
